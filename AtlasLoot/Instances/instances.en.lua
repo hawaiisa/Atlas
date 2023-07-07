f39d927c1994ac74d0a964b656915f5d31919e75
Hawaiisa <havardsmalas@gmail.com>
2023-07-07 23:09:20 +0200

    AtlasLoot v5.03.24: Azus trinket added

--- AtlasLoot/Instances/instances.en.lua
+++ AtlasLoot/Instances/instances.en.lua
@@ -1647,6 +1647,7 @@ AtlasLoot_Data["AtlasLootItems"] = {
 		{ 26194, "INV_Misc_MonsterClaw_03", "=q4=Golden Ceremonial Bloodletter", "=ds=#h3#, #w13#", "", "", "", "", "", "", "", "", "", "", "", true },
 		{ 26202, "INV_Helmet_08", "=q4=Helm of Sacrifice", "=ds=#s1#, #a4#", "", "", "", "", "", "", "", "", "", "", "", true },
 		{ 26203, "INV_Jewelry_Talisman_04", "=q4=Mark of Thirst", "=ds=#s14#", "", "", "", "", "", "", "", "", "", "", "", true },
+		{ 26208, "INV_Potion_49", "=q4=Berserker\'s Blood Sample", "=ds=#s14#", "", "", "", "", "", "", "", "", "", "", "", true },
 	};
 	ZGHermit = {
 		{ 19721, "INV_Shoulder_19", "=q4=Primal Hakkari Shawl", "=q16=#c4#=ds=, =q14=#c2#=ds=, =q10=#c3#", },
