046e2a7fdfcc9e42a6d1d6d521801846740b71cc
Hawaiisa <hawaiisa@gmail.com>
2026-07-07 17:28:21 +0200

    AtlasLoot v5.06.04: Bump version numbers.

--- AtlasLoot/Core/AtlasLoot.lua
+++ AtlasLoot/Core/AtlasLoot.lua
@@ -76,10 +76,10 @@ local BIS = AceLibrary("Babble-ItemSet-2.2")
 --Establish version number and compatible version of Atlas
 local VERSION_MAJOR = "5";
 local VERSION_MINOR = "06";
-local VERSION_BOSSES = "03";
+local VERSION_BOSSES = "04";
 ATLASLOOT_VERSION = "|cffFF8400AtlasLoot Enhanced for Vanilla+ v"..VERSION_MAJOR.."."..VERSION_MINOR.."."..VERSION_BOSSES.."|r";
 ATLASLOOT_CURRENT_ATLAS = "2.2.1";
-ATLASLOOT_PREVIEW_ATLAS = "2.1.7";
+ATLASLOOT_PREVIEW_ATLAS = "2.2.0";
 
 --Compatibility with old EquipCompare/EQCompare
 ATLASLOOT_OPTIONS_EQUIPCOMPARE = AL["Use EquipCompare"];
